f6bc83c4f462babc3c4c12232d891337b1d047e6
asharma <anish.sharma@mavorion.com>
2025-09-15 16:30:13 +0545

    fix: minor css fixed for logout button on navbar

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header className="bg-gray-800 text-white shadow">
       <div className="max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8 flex justify-between">
         <h1 className="text-3xl font-bold">Hardware Store</h1>
-        <button onClick={logout} className="text-red-600 hover:text-red-800">
+        <button onClick={logout} className="text-red-600 hover:text-red-800 hover:cursor-pointer">
             Logout
         </button>
       </div>
