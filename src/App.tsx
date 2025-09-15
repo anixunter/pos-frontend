3d24021effcbac4d6f633316f7ec01246a8ded53
asharma <anish.sharma@mavorion.com>
2025-09-15 12:07:28 +0545

    chore: removed react query and its related setups

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { QueryClientProvider } from "@tanstack/react-query"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { queryClient } from '@/services/queryClient'
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
           
         </Routes>
       </BrowserRouter>
-    </QueryClientProvider>
   )
 }
 
