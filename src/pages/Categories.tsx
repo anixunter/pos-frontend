57ff0c8dffd579e9a4159516d738310d590f05b1
asharma <anish.sharma@mavorion.com>
2025-09-16 12:52:56 +0545

    feat: category table action added, and required shadcn components added

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,6 +1,15 @@
 import type { ColumnDef } from "@tanstack/react-table"
 import { useEffect } from "react"
 import { useCategoryStore } from "@/stores/categoryStore";
+import { DataTable } from "@/components/ui/data-table";
+import { MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export type Category = {
   id: string;
@@ -17,6 +26,27 @@ export const columns: ColumnDef<Category>[] = [
     accessorKey: "description",
     header: "Description",
   },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const category = row.original
+ 
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              {/* <span className="sr-only">Open menu</span> */}
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => navigator.clipboard.writeText(category.id)}>Copy Id</DropdownMenuItem>
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },
 ]
 
 const Categories = () => {
@@ -32,13 +62,9 @@ const Categories = () => {
   return (
     <div>
       <h2 className="text-2xl font-semibold mb-4">Categories Management</h2>
-      <ul>
-        {categories.map((category) => (
-          <li key={category.id}>
-            <strong>{category.name}</strong> - {category.description}
-          </li>
-        ))}
-      </ul>
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={categories} />
+      </div>
     </div>
   );
 };
